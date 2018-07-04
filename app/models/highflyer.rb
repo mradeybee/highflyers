6969e4a32e65671b93ca3d273f120ea741e87a70
Adepoju Adebayo <mradeybee@gmail.com>
2018-07-04 19:03:44 +0100

    fix test

--- app/models/highflyer.rb
+++ app/models/highflyer.rb
@@ -12,7 +12,7 @@ class Highflyer < Student
 
   def self.rate_teacher(teacher_id:, rating:, number:)
     teacher = Teacher.find_by_id(teacher_id)
-    student_id = at_possision(number)&.id
+    student_id = at_possision(number).try(:id)
     rating_point = rating.to_i <= 2 ? rating.to_i * 2 : 5
     TeacherRating.create(student_id: student_id, teacher: teacher, rating: rating_point)
   end
