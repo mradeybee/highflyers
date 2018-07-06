ec03bb68a137d415c09dd4b77b887d6e768c13d0
Adepoju Adebayo <mradeybee@gmail.com>
2018-07-06 13:14:42 +0100

    fix indentation

--- app/models/highflyer.rb
+++ app/models/highflyer.rb
@@ -1,10 +1,11 @@
 class Highflyer < Student
   default_scope {
-    joins(:student_courses).joins(teachers: :teacher_ratings)
-    .having('AVG(teacher_ratings.rating) >= 1')
-    .group('students.id, student_courses.id')
-    .where('student_courses.score >= 80')
-    .order('student_courses.score')
+    joins(:student_courses)
+      .joins(teachers: :teacher_ratings)
+      .having('AVG(teacher_ratings.rating) >= 1')
+      .group('students.id, student_courses.id')
+      .where('student_courses.score >= 80')
+      .order('student_courses.score')
   }
 
   def self.at_position(number)
