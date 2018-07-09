d78b6390993f4bdbdb2de492c1a789870f42db9c
Adepoju Adebayo <mradeybee@gmail.com>
2018-07-09 11:22:23 +0100

    more updates

--- app/serializers/teacher_serializer.rb
+++ app/serializers/teacher_serializer.rb
@@ -1,13 +1,25 @@
 class TeacherSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :created_at, :updated_at
 
-  has_many :courses
-  has_many :teacher_ratings
   attribute :average_rating
+  attribute :teacher_ratings
   attribute :students
+  attribute :courses
 
   def average_rating
     object.teacher_ratings.average(:rating).to_f
+  end
+
+  def teacher_ratings
+    object.teacher_ratings.map do |rating|
+      student = rating.student
+      {
+        id: rating.id, rating: rating.rating,
+        submited_by: { id: student.id,
+                       name: student.name,
+                       email: student.email }
+      }
+    end
   end
 
   def students
@@ -16,6 +28,16 @@ class TeacherSerializer < ActiveModel::Serializer
         id: student.id,
         name: student.name,
         email: student.email
+      }
+    end
+  end
+
+  def courses
+    course = object.courses.map do |course|
+      {
+        id: course.id,
+        name: course.name,
+        code: course.code
       }
     end
   end
